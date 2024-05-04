6aa3f4b4819061a852726b900f878e5859806b7b
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-05-05 00:01:44 +0530

    Create chatbotpage.js

---
+++ chatbotpage.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+
+function MyComponent() {
+  return (
+    <div className="flex flex-col pb-20 bg-white">
+      <div className="flex gap-5 px-14 pt-3.5 pb-1.5 w-full bg-red-950 max-md:flex-wrap max-md:px-5 max-md:max-w-full">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/faef91ece89553e9f3bbb2395e60d71c59ac60e001f52c152983e5ab3001a287?"
+          className="shrink-0 self-start aspect-[1.01] w-[95px]"
+        />
+        <div className="flex-auto max-md:max-w-full">
+          <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+            <div className="flex flex-col w-[67%] max-md:ml-0 max-md:w-full">
+              <div className="flex gap-5 justify-between items-start max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/cf5f023d9a5514baafa3fdf7c82725eeb8c62ca463fd23cb2c8a99f5c9c68834?"
+                  className="shrink-0 max-w-full aspect-[1.22] w-[114px]"
+                />
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/2ce2e7eb71c9ec00d6bbc6fa1ae12d30d7ac5530d0d84c338c8c369ef7feca05?"
+                  className="shrink-0 mt-1 max-w-full aspect-[1.15] w-[109px]"
+                />
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/404bf9e45643ebf1ae0a60b3238c950a13365816c44b950cd8277eeba80554a9?"
+                  className="shrink-0 self-stretch my-auto max-w-full aspect-[1.2] w-[103px]"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+              <div className="flex grow gap-5 mt-1 max-md:mt-10">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/146b09a5b9d19a43e1a0d618aefe9538e95706eb5acc3d57030c9b3159934dc0?"
+                  className="shrink-0 max-w-full aspect-[1.1] w-[111px]"
+                />
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/522e80a427e24207f97bc015609bfb7f73c20d2503642922c9c0f1929c77205c?"
+                  className="shrink-0 self-start aspect-[1.02] w-[90px]"
+                />
+              </div>
+            </div>
+          </div>
+   
